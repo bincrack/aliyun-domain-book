193ed5ec629112bfdc14836d54bb858ff44ca43f
xiongbin <xiongbin@jiheit.com>
2018-06-25 14:22:26 +0800

    -

---
+++ js/content-script.js
@@ -0,0 +1,12 @@
+﻿$(function() {
+	var token = null;
+	var inject = document.createElement('script');
+	inject.type = "text/javascript";
+	inject.src = chrome.extension.getURL('js/inject.js');
+	inject.onload = function() {
+		this.remove();
+	};
+	document.body.appendChild(inject);
+	
+	console.log("content-script...");
+});
---
+++ js/inject.js
@@ -0,0 +1,65 @@
+$(function() {
+	var suffixs = ["com", "net", "org", "cc", "cn"];
+	var token = window.um.getToken();
+	var check = document.createElement("a");
+	check.href = "javascript:;"
+	check.innerText = "check";
+	check.onclick = function() {
+		$("div.table table tbody tr td[data-field='domain_name']").map(function() {
+			var td = this;
+			var item = $(td).find("input[type='checkbox']").val();
+			var name = item.substring(0, item.lastIndexOf("."));
+			var d = new Date();
+			var div = $(td).find("div.other");
+			if (div.length > 0) {
+				div.remove();
+			}
+			div = document.createElement("div");
+			div.className = "other";
+			suffixs.map(function(suffix) {
+				var callback = "jsonp_" + d.getTime() + "_" + Math.floor(Math.random() * 100000);
+				var url = "https://checkapi.aliyun.com/check/checkdomain?domain=" + name + "." + suffix + "&command=&token=" + token + "&ua=&currency=&site=&bid=&_csrf_token=&callback=" + callback;
+				var script = document.createElement("script");
+				script.type = "text/javascript";
+				script.src = url;
+				document.body.appendChild(script);
+
+				var span = $(td).find("span." + suffix);
+				if (span.length > 0) {
+					span.remove();
+				}
+
+				span = document.createElement("span");
+				span.id = callback;
+				span.className = "com";
+				$(div).append(span);
+
+				eval(callback + " = function(data) {checkdomain('" + callback + "', data, '" + suffix + "')}");
+			});
+			$(td).append(div);
+		})
+	};
+	$("div.table table thead tr th[data-field='domain_name']").append(check);
+	$("div.batch").append(check);
+
+	console.log("content-inject...");
+});
+function checkdomain(id, data, suffix) {
+	var color = "#333";
+	if (data && data.errorCode == 0) {
+		if (data.module[0].avail == 0) {
+			color = "#777";
+		} else if (data.module[0].avail == 1) {
+			color = "#5cb85c";
+		} else if (data.module[0].avail == 4) {
+			color = "#5cb85c";
+		} else if (data.module[0].avail == 5) {
+			color = "#777";
+		} else {
+			suffix += "：" + data.module[0].avail;
+			color = "#ff5029";
+		}
+	}
+
+	$("#" + id).css({"margin-left": "5px", "padding": "0px 5px", "color": "#fff", "background": color}).text(suffix);
+}
